56f36b1eea9574f52564dad0fe1d11865d4fcbad
Bram <brdv@pm.me>
2024-05-24 21:30:02 +0200

    refactor list command a bit

--- src/list.rs
+++ src/list.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::Path};
+use std::fs::{self, DirEntry};
 
 use crate::{get_rtodo_done_location, get_rtodo_todo_location, todo::Todo, ListArgs};
 
@@ -22,61 +22,87 @@ fn get_todos(done: bool) -> Vec<Todo> {
 
     get_files_from(location.as_str())
         .into_iter()
-        .filter_map(|file| parse_path_str(file.as_str()))
+        .filter_map(|file| parse_entry(&file))
         .collect()
 }
 
-fn get_files_from(location: &str) -> Vec<String> {
-    let location_path = Path::new(location);
+fn get_files_from(location: &str) -> Vec<DirEntry> {
+    let paths = fs::read_dir(location)
+        .expect(format!("Given location doesnt exist on machine, {}", location).as_str());
 
-    let mut file_names: Vec<String> = Vec::new();
-
-    if location_path.exists() && location_path.is_dir() {
-        if let Ok(entries) = fs::read_dir(location_path) {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    if let Some(file_name) = entry.path().to_str() {
-                        file_names.push(file_name.to_string());
-                    }
-                }
+    paths
+        .filter_map(|entry| {
+            if let Ok(path) = entry {
+                return Some(path);
             }
+            None
+        })
+        .collect()
+}
+
+fn parse_entry(entry: &DirEntry) -> Option<Todo> {
+    if is_valid_todo_string(entry.file_name().to_str()?) {
+        let path = entry.path();
+        if let Some(path_str) = path.to_str() {
+            return Some(Todo::from_path(path_str));
+        } else {
+            eprintln!("Could not convert path to string: {:?}", path);
         }
     }
 
-    file_names
-}
-
-fn parse_path_str(path: &str) -> Option<Todo> {
-    let file = Path::new(path).file_name().unwrap().to_str().unwrap();
-
-    if is_valid_todo_string(file) {
-        return Some(Todo::from_path(path));
-    }
     None
 }
 
 fn is_valid_todo_string(string: &str) -> bool {
     let split: Vec<&str> = string.split(".").collect();
 
-    if split.len() < 4 {
-        return false;
+    split.len() == 4
+        // id is valid u32
+        && split[0].trim().parse::<u32>().is_ok()
+        // task is not empty
+        && !split[1].trim().is_empty()
+        // contains rtodo suffix
+        && split[2].trim() == "rtodo"
+        // is md file
+        && split[3].trim() == "md"
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::list::is_valid_todo_string;
+
+    #[test]
+    fn test_valid_todo_str() {
+        let task = "0.task.rtodo.md";
+
+        assert!(is_valid_todo_string(task));
     }
 
-    if split[0].trim().parse::<u32>().is_err() {
-        return false;
+    #[test]
+    fn test_no_rtodo_suffix() {
+        let task = "0.task.todor.md";
+
+        assert!(!is_valid_todo_string(task));
     }
 
-    if split[1].trim().is_empty() {
-        return false;
+    #[test]
+    fn test_no_md_file() {
+        let task = "0.task.rtodo";
+
+        assert!(!is_valid_todo_string(task));
     }
 
-    if split[2].trim() != "rtodo" {
-        return false;
+    #[test]
+    fn test_id_no_u32() {
+        let task = "x.task..rtodo.md";
+
+        assert!(!is_valid_todo_string(task));
     }
 
-    if split[3].trim() != "md" {
-        return false;
-    }
+    #[test]
+    fn test_too_short() {
+        let task = "0.task.md";
 
-    true
+        assert!(!is_valid_todo_string(task));
+    }
 }
